88cb35a2e4eadcb3a10e509c3acd9ea8d3efbcb1
ryleehernandez <rylhernandez@csumb.edu>
2021-02-09 18:51:39 -0800

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,29 @@
+//Everyday Task Pseudocode- Morning Routine:
+
+//Wake up
+  //At around 8am
+//Scroll through social media applications
+  //Til about 8:30am
+//Go into bathroom; Wash face and brush teeth
+//Change out of pajamas
+  //Change into gym attire
+//Drink pre-workout energy drink
+  //Celsius: Watermelon
+//Go to the gym
+//Come home and make breakfast & iced coffee
+  //Avocado toast with an egg
+//Eat breakfast
+
+//Computer Task Pseudocode- Uploading ode to the Server
+
+//Write HTML code in Atom
+//Save it
+//Copy full path
+//Paste into browser
+  //ensure it looks correct
+//Open to Github
+//Write a brief description
+  //Write title and type of file
+//Click “Commit to Master”
+//Click “Push Origin”
+//Type in url to make sure it is uploaded and working
