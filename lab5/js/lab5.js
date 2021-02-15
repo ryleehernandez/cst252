553c48ae94a27f32a99ec8ecd87a60041421fef7
ryleehernandez <rylhernandez@csumb.edu>
2021-02-14 17:27:51 -0800

    lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,27 @@
+/*
+  * Author: Rylee Hernandez <rylhernandez@csumb.edu>
+  * Created: 14 February 2021
+  * License: Public Domain
+*/
+
+// Define Variables
+var make = "Volkswagen";
+var model = "Jetta";
+var color = "white";
+var year = "2016";
+
+//calculate
+var age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+
+
+
+var ownIt = true;
+
+var age = new Date().getFullYear();
